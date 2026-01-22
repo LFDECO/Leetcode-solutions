1005c995850ad702d8ecde718373137d88d9dffc
LFDECO <122837986+LFDECO@users.noreply.github.com>
2026-01-22 19:16:41 +0530

    Time: 4 ms (66.78%), Space: 19.9 MB (8.55%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        pairs = list(zip(names, heights))
+        pairs.sort(key=lambda x: x[1], reverse=True)
+
+        result = []
+        for name, height in pairs:
+            result.append(name)
+
+        return result
