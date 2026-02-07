87aa0b87f1b9b6cf0040ccba451a2deb20df67e8
LFDECO <122837986+LFDECO@users.noreply.github.com>
2026-02-07 18:36:46 +0530

    Time: 0 ms (100%), Space: 21.3 MB (28.28%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=nums.count(i)
+        return max(d,key=d.get)
