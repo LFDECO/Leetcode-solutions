b9086b821cf2dc69ede4bbf9758cb5190921c7dd
Siddhartha Bhaduri <122837986+LFDECO@users.noreply.github.com>
2026-03-11 20:48:10 +0530

    Time: 47 ms (5.35%), Space: 23.5 MB (95.19%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        running_sum=0
+        count=0
+        map1={}
+        map1[0]=1
+        for i in range(len(nums)):
+            running_sum+=nums[i]%k
+            if running_sum%k in map1:
+                count+=map1[running_sum%k]
+            if running_sum%k in map1:
+                map1[running_sum%k]+=1
+            else:
+                map1[running_sum%k]=1
+        return count
