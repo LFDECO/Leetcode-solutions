b33ba7747248a2ade47c9891b7c82d586700607c
Siddhartha Bhaduri <122837986+LFDECO@users.noreply.github.com>
2026-03-21 13:59:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,32 +1,31 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        window_size=len(p)
-        map1={}
-        map2={}
-        result=[]
+        map1 = {}
+        map2 = {}
+        result = []
+        window_size = len(p)
         if len(p)>len(s):
             return []
         for i in p:
-            if i not in map1:
-                map1[i]=1
+            if i in map1:
+                map1[i] += 1
             else:
-                map1[i]+=1
+                map1[i] = 1
         for i in range(window_size):
-            if s[i] not in map2:
-                map2[s[i]]=1
+            if s[i] in map2:
+                map2[s[i]] += 1
             else:
-                map2[s[i]]+=1
-        if map1==map2:
-            result.append(i-window_size+1)
-        for i in range(window_size,len(s)):
-            if s[i] not in map2:
-                map2[s[i]]=1
+                map2[s[i]] = 1
+            if map1 == map2:
+                result.append(i - window_size + 1)
+        for i in range(window_size, len(s)):
+            if s[i] in map2:
+                map2[s[i]] += 1
             else:
-                map2[s[i]]+=1
-            map2[s[i-window_size]]-=1
-            if map2[s[i-window_size]]==0:
-                del map2[s[i-window_size]]
-            if map1==map2:
-                result.append(i-window_size+1)
+                map2[s[i]] = 1
+            map2[s[i - window_size]] -= 1
+            if map2[s[i - window_size]] == 0:
+                del map2[s[i - window_size]]
+            if map1 == map2:
+                result.append(i - window_size + 1)
         return result
-
