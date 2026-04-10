79979897097081eb17426acfdd05c46da55944de
Siddhartha Bhaduri <122837986+LFDECO@users.noreply.github.com>
2026-04-10 19:37:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,24 +1,23 @@
 import math
+
+
 class Solution:
-    def can(self,mid,h,piles):
-        hours=0
+    def can(self, mid, h, piles):
+        hours = 0
         for i in piles:
-            hours+=math.ceil(i/mid)
-        if hours>h:
+            hours += math.ceil(i / mid)
+        if hours > h:
             return False
         else:
             return True
+
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        low=1
-        high=max(piles)
-        while low<=high:
-            mid=low+(high-low)//2
-            if self.can(mid,h,piles):
-                high=mid-1
+        low = 1
+        high = max(piles)
+        while low <= high:
+            mid = low + (high - low) // 2
+            if self.can(mid, h, piles):
+                high = mid - 1
             else:
-                low=mid+1
+                low = mid + 1
         return low
-
-
-
-        
