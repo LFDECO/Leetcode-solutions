42979e956e179dd9152565261e300dcef3246997
Siddhartha Bhaduri <122837986+LFDECO@users.noreply.github.com>
2026-06-04 02:58:51 +0530

    Time: 1099 ms (5.4%), Space: 22.8 MB (81.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        map1={}
-        target=1
-        res=[]
+        map1 = {}
+        target = 1
+        res = []
         for i in nums:
             if i not in map1:
-                map1[i]=1
+                map1[i] = 1
             else:
-                map1[i]+=1
+                map1[i] += 1
         map1 = dict(sorted(map1.items(), key=lambda x: x[1], reverse=True))
-        while target<=k:
+        while target <= k:
             for i in map1:
                 if i in res:
                     continue
                 else:
                     res.append(i)
                     break
-            target+=1
+            target += 1
         return res
