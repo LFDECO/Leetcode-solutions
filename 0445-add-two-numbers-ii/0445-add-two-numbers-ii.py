734045fe7895c89dd96023691d2217027505ed18
Siddhartha Bhaduri <122837986+LFDECO@users.noreply.github.com>
2026-06-17 12:59:43 +0530

    Time: 4 ms (59.27%), Space: 19.5 MB (14.21%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -4,25 +4,26 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        arr1=[]
-        arr2=[]
-        curr1=l1
-        curr2=l2
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        arr1 = []
+        arr2 = []
+        curr1 = l1
+        curr2 = l2
         while curr1:
             arr1.append(str(curr1.val))
-            curr1=curr1.next
+            curr1 = curr1.next
         while curr2:
             arr2.append(str(curr2.val))
-            curr2=curr2.next
-        x1=int("".join(arr1))
-        x2=int("".join(arr2))
-        sum1=x1+x2
-        res=list(str(sum1))
-        dummy=ListNode()
-        res1=dummy
+            curr2 = curr2.next
+        x1 = int("".join(arr1))
+        x2 = int("".join(arr2))
+        sum1 = x1 + x2
+        res = list(str(sum1))
+        dummy = ListNode()
+        res1 = dummy
         for i in res:
-            res1.next=ListNode(int(i))
-            res1=res1.next
+            res1.next = ListNode(int(i))
+            res1 = res1.next
         return dummy.next
-            
