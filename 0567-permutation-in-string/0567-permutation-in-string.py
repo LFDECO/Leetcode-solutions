de640335285e62b8dd7f02760ea5e23b066ac578
Siddhartha Bhaduri <122837986+LFDECO@users.noreply.github.com>
2026-07-02 20:43:13 +0530

    Time: 6572 ms (5.01%), Space: 19.3 MB (85.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,14 +1,14 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        k=len(s1)
-        window=[]
+        k = len(s1)
+        window = []
         if len(s2) < len(s1):
             return False
         for i in range(k):
             window.append(s2[i])
         if "".join(sorted(list(s1))) in "".join(sorted(window)):
             return True
-        for i in range(k,len(s2)):
+        for i in range(k, len(s2)):
             window.append(s2[i])
             window.pop(0)
             if "".join(sorted(list(s1))) in "".join(sorted(window)):
