27fd43b89d216aa800d1a9c0d83f10cac6e351b7
Siddhartha Bhaduri <122837986+LFDECO@users.noreply.github.com>
2026-07-30 21:13:45 +0530

    Time: 0 ms (100%), Space: 19.4 MB (16.35%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n = len(word)
+        pushes = 0
+        
+        # Add pushes based on 8-slot blocks
+        pushes += min(n, 8) * 1
+        if n > 8:
+            pushes += min(n - 8, 8) * 2
+        if n > 16:
+            pushes += min(n - 16, 8) * 3
+        if n > 24:
+            pushes += (n - 24) * 4
+            
+        return pushes
