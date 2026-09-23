fc9979332a988f7271888db5284e0c0d8cadbc9d
Siddhartha Bhaduri <122837986+LFDECO@users.noreply.github.com>
2026-09-23 20:05:28 +0530

    Time: 31 ms (78.31%), Space: 14.8 MB (48.55%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,25 @@
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
         map1={}
+        map1[0]=1
         running_sum=0
         count=0
-        map1[0]=1
-        for j in range(len(nums)):
-            running_sum+=nums[j]
+        for i in nums:
+            running_sum+=i
             if running_sum-k in map1:
-                count+=map1[running_sum-k]
-        
-            
+              count+=map1[running_sum-k]
             if running_sum in map1:
                 map1[running_sum]+=1
             else:
                 map1[running_sum]=1
+        
         return count
+
+
 
